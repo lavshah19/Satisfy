95fc1217e9eca5c830c4b88b44b81a91846f4c0f
Lav Shah <125074649+lavshah19@users.noreply.github.com>
2024-10-28 00:48:07 -0500

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,11 +9,11 @@ const Navbar = () => {
   return (
     <nav className="bg-indigo-600 text-white py-6 px-2 md:p-6 shadow-md sticky top-0 w-full z-20">
       <div className="container mx-auto flex flex-wrap items-center justify-between">
-        <h1 className=" text-md md:text-2xl font-bold">
+        <h1 className="sm:text-md md:text-2xl font-bold">
           <span className="text-yellow-500">Satisfy</span> Trading & Services W.L.L
         </h1>
         <button
-          className="text-white md:hidden"
+          className="text-white md:hidden text-2xl"
           onClick={() => setIsOpen(!isOpen)}
           aria-expanded={isOpen}
           aria-controls="navbar-menu"
