c01f3ce76a49d295941a50287edd6fc912521957
Lav Shah <125074649+lavshah19@users.noreply.github.com>
2024-10-27 23:44:10 -0500

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,6 @@
 // src/components/Home.jsx
 import React from 'react';
-import homeImg from '../assets/homeimg.jpg'; // Adjust path as needed
+
 import About from './About';
 import Services from './Services';
 import Contact from './Contact';
@@ -52,7 +52,7 @@ const Home = () => {
         {/* Right Image Section */}
         <div className="md:w-1/2 mt-8 md:mt-0 flex justify-center md:justify-end">
   <div className="w-full md:w-3/4 h-80 md:h-96 flex items-center justify-center shadow-lg rounded-lg overflow-hidden">
-    <img src={homeImg} alt="Introductory" className="w-full h-full object-cover" />
+    <img src="https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Introductory" className="w-full h-full object-cover" />
   </div>
 </div>
 
